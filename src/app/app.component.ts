cfa2998660219477a7fb7c7d80a3d340b973d0ff
Kenneth Yuen <kenneth.x.yuen@gmail.com>
2017-07-23 13:48:20 -0500

    Added Customer Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
 })
 export class AppComponent  {
-  
+   
   // [] means property binding : Component to DOM
   // () means event binding: DOM to Component
 
   title = 'Angular Playground'
-  name = 'Jo'; 
+  name = 'Stranger'; 
   color ='blue';
+  customers =[
+    {id: 1, name: 'Mickey'},
+    {id: 2, name: 'Minnie'},
+    {id: 3, name: 'Goofy'},
+    {id: 4, name: 'Daffy'},
+    {id: 5, name: 'Donald'},
+  ];
 
   changeColor() {
     this.color = this.color === 'blue' ? 'red' : 'blue';
