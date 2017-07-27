4afb8a7a7195a6716dc26c990921f0831812e684
Kenneth Yuen <kenneth.x.yuen@gmail.com>
2017-07-27 10:41:47 -0500

    externalized customer into a json and integrated with Http

--- src/app/customer/customer.service.ts
+++ src/app/customer/customer.service.ts
@@ -1,19 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from "@angular/http";
+
+const URL_CUSTOMER = 'app/customers.json';
 
 @Injectable()
+
 export class CustomerService {
 
-    constructor() { }
+    constructor(private _http: Http) { }
 
     getCustomers() {
-        return [
-            {id: 1, name: 'Mickey'},
-            {id: 2, name: 'Minnie'},
-            {id: 3, name: 'Goofy'},
-            {id: 4, name: 'Daffy'},
-            {id: 5, name: 'Donald'},
-        ];
-
+        return this._http.get(URL_CUSTOMER).map((response: Response) => response.json());
     }
 
 }
--- src/app/customer/customers.component.ts
+++ src/app/customer/customers.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+
 import { CustomerService} from './customer.service';
 
 @Component({
     selector: 'app-customers',
     templateUrl: 'app/customer/customers.component.html',
-    providers:    [CustomerService]
+    providers:    [CustomerService],
 })
 
 export class CustomersComponent implements OnInit {
-    customers: any[];
+    customers: Observable<any[]>;
 
     constructor(private _customerService: CustomerService) {  }
 
